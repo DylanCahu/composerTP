90416357c1fe4ec2e0195b95bad3dbcbf3f7e336
DylanCahu <dylancahu55@gmail.com>
2020-03-23 15:51:05 +0100

    [Core] Add Twig package

--- src/index.php
+++ src/index.php
@@ -8,3 +8,16 @@ $logger = new Logger('main');
 $logger->pushHandler(new StreamHandler(__DIR__ . '/../log/app.log', Logger::DEBUG));
 
 $logger->info('First message');
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+
+$twig = new \Twig\Environment($loader, [
+    'cache' => '../cache',
+    ]);
+
+    echo $twig->render('base.html.twig',
+    [
+    'title' => 'Essai de Twig',
+    'text' => 'Texte inséré dans la page.',
+    ]
+    );
